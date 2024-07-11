187baf85e3f6a0b86302690dc7145aa40a38c6b5
demola <ademolaabds@gmail.com>
2024-07-11 16:50:50 +0100

    done

--- task-one/src/app.js
+++ task-one/src/app.js
@@ -3,16 +3,13 @@ import FileTree from "./fileTree";
 export function createFileTree(input) {
   const fileTree = new FileTree();
  
-  // Sort input nodes by ascending order of id.
-  // This ensures the parent node for a child node can be found while looping.
+ 
   const ascendingSortedInput = input
     .slice(0, 1)
     .concat(input.slice(1).toSorted((a, b) => a.id - b.id));
  
   for (const inputNode of ascendingSortedInput) {
-    const parentNode = inputNode.parentId
-      ? fileTree.findNodeById(inputNode.parentId)
-      : null;
+    const parentNode = inputNode.parentId ? fileTree.findNodeById(inputNode.parentId) : null;
  
     fileTree.createNode(
       inputNode.id,
